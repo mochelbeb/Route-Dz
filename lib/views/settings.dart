18c690c8a7db3db83879383d0486b862f6865007
katia <akbinourelhouda@gmail.com>
2023-02-24 20:15:08 +0100

    ,,

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -4,64 +4,69 @@ import 'package:gap/gap.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Styles.bgColor,
-        body:ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 40 , top: 50 ,right: 40 ),
-              child: Text("Paramètres" , style: Styles.headlineStyle1,),
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 40, top: 50, right: 40),
+            child: Text(
+              "Paramètres",
+              style: Styles.headlineStyle1,
             ),
-            const Gap(30),
-            colorTiles(),
-          ],
-        ),
-    ) ;
+          ),
+          const Gap(30),
+          Column(
+            children: [
+              _colorTile(FontAwesomeIcons.user, "Compte"),
+              _divider(),
+              _colorTile(FontAwesomeIcons.language, "Changer de langue"),
+              _divider(),
+              _colorTile(FontAwesomeIcons.file, "Conditions"),
+              _divider(),
+              _colorTile(FontAwesomeIcons.lock, "Politique de confidentialité"),
+              _divider(),
+              _colorTile(FontAwesomeIcons.circleInfo, "À propos"),
+              _divider(),
+              _colorTile(FontAwesomeIcons.rightFromBracket, "Se déconnecter"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _divider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: const Divider(
+        thickness: 1.5,
+      ),
+    );
+  }
+
+  Widget _colorTile(IconData icon, String text) {
+    return ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 40, vertical: 3),
+      leading: Icon(
+        icon,
+        color: Styles.textColor,
+        size: 15,
+      ),
+      title: Text(
+        text,
+        style: Styles.textStyle.copyWith(color: Styles.textColor),
+      ),
+      trailing: Icon(
+        FontAwesomeIcons.chevronRight,
+        color: Styles.textColor,
+        size: 15,
+      ),
+    );
   }
 }
-
-Widget divider(){
-  return const Padding(
-    padding:  EdgeInsets.symmetric(horizontal: 40),
-    child: Divider(
-      thickness: 1.5,
-    ),
-    );
-}
-
-Widget colorTiles (){
-  return Column(
-    children: [
-      colorTile(FontAwesomeIcons.user,"Compte"),divider(),
-      colorTile(FontAwesomeIcons.language, "Change de langue"),divider(),
-      colorTile(FontAwesomeIcons.file, "Conditions"),divider(),
-      colorTile(FontAwesomeIcons.lock, "Politique de confidentialité"),divider(),
-      colorTile(FontAwesomeIcons.circleInfo, "À propos"),divider(),
-      colorTile(FontAwesomeIcons.rightFromBracket, "Se déconnecter"),
-    ],
-  );
-}
-
-Widget colorTile (IconData icon , String text){
-  return ListTile(
-    contentPadding: EdgeInsets.symmetric(horizontal: 40 , vertical: 3),
-    leading : Icon(
-      icon , 
-      color: Styles.textColor,
-      size: 15
-      ),
-    title : Text(
-      text , 
-      style: Styles.textStyle.copyWith(color: Styles.textColor),
-    ),
-    trailing: Icon(
-      FontAwesomeIcons.chevronRight,  
-      color: Styles.textColor, 
-      size: 15
-      ),
-  );
-}
-    
